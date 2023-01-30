e734c67c38b011dcc7e47b0c0fac4cc98d0b5185
MD MAHSIN UL ISLAM <mahsin.islam@gmail.com>
2023-01-30 08:56:21 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import chair from '../../../assets/images/chair.png';
+import bg from '../../../assets/images/bg.png';
+import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen"
+        style={{
+            backgroundImage: `url(${bg})`
+          }}
+          >
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt="" />
+                <div>
+                    <h1 className="text-5xl font-bold">Your New Smile Starts Here!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <PrimaryButton>Get Started</PrimaryButton>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
